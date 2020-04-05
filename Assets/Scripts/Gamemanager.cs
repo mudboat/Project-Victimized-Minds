f8f480b0599451d46a260994b62f4b387a151412
Armouredwasp <48547220+Armouredwasp@users.noreply.github.com>
2020-04-05 17:03:35 +0900

    iroiro

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gamemanager : MonoBehaviour
+{
+    public Text Dialoguetext;
+    public Text Nametext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Nametext.text = StringClass.CharacterText0;
+        Dialoguetext.text = StringClass.DialogueText0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StringClass.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringClass
+{
+    public static string CharacterText0 = "Dr.Shnoop";
+    public static string DialogueText0 = "Fuck you";
+}
